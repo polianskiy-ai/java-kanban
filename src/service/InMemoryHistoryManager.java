9de938f68b91247dd8675d51ed64e4faf8930e67
aligpo <polianskiy-ai@yandex.ru>
2023-03-12 22:00:10 +0300

    fix: исправление замечаний 1.1

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -13,23 +13,23 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void add(Task task) {
         if (task != null) {
+            removeNode(history.get(task.getId()));
+            linkLast(task);
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        removeNode(history.remove(id));
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
+    }
+
+    public void linkLast(Task task) {
             Node node = new Node(task);
-            Node existingNode = history.get(task.getId());
-            if (existingNode != null) {
-                Node prev = existingNode.prev;
-                Node next = existingNode.next;
-                if (prev != null) {
-                    prev.next = next;
-                } else {
-                    head = next;
-                }
-                if (next != null) {
-                    next.prev = prev;
-                } else {
-                    tail = prev;
-                }
-                size--;
-            }
             if (size == 0) {
                 head = node;
             } else {
@@ -39,35 +39,27 @@ public class InMemoryHistoryManager implements HistoryManager {
             tail = node;
             size++;
             history.put(task.getId(), node);
+    }
+
+    public void removeNode(Node node) {
+        if (node != null) {
+            Node prev = node.prev;
+            Node next = node.next;
+            if (prev != null) {
+                prev.next = next;
+            } else {
+                head = next;
+            }
+            if (next != null) {
+                next.prev = prev;
+            } else {
+                tail = prev;
+            }
+            size--;
         }
     }
 
-
-
-    @Override
-    public void remove(int id) {
-        Node node = history.get(id);
-        if (node == null) {
-            return;
-        }
-        Node prev = node.prev;
-        Node next = node.next;
-        if (prev != null) {
-            prev.next = next;
-        } else {
-            head = next;
-        }
-        if (next != null) {
-            next.prev = prev;
-        } else {
-            tail = prev;
-        }
-        size--;
-        history.remove(id);
-    }
-
-    @Override
-    public List<Task> getHistory() {
+    public List<Task> getTasks() {
         List<Task> tasks = new LinkedList<>();
         Node newNode = head;
         while (newNode != null) {
